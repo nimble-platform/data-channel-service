6d6d0098bfc1098a8e3a435f22c5e8bdbc509c04
Johannes Innerbichler <j.innerbichler@gmail.com>
2018-06-20 14:26:41 +0200

    cleaned code

--- data-channel-service/src/main/java/eu/nimble/service/datachannel/controller/EpcController.java
+++ data-channel-service/src/main/java/eu/nimble/service/datachannel/controller/EpcController.java
@@ -1,18 +1,14 @@
 package eu.nimble.service.datachannel.controller;
 
 import com.mashape.unirest.http.exceptions.UnirestException;
-import eu.nimble.service.datachannel.entity.ChannelConfiguration;
 import eu.nimble.service.datachannel.entity.tracing.EpcCodes;
 import eu.nimble.service.datachannel.identity.IdentityClient;
-import eu.nimble.service.datachannel.kafka.KafkaDomainClient;
 import eu.nimble.service.datachannel.repository.EpcCodesRepository;
 import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import request.CreateChannel;
 
 import java.io.IOException;
 import java.util.Set;
@@ -33,12 +29,12 @@ public class EpcController {
     @Autowired
     private IdentityClient identityClient;
 
-    @ApiOperation(value = "Register EPC codes for an order.", nickname = "registerEPCCodes")
+    @ApiOperation(value = "Register EPC codes for an order.", nickname = "registerEpcCodes")
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = "EPC codes registered"),
             @ApiResponse(code = 400, message = "Error while registering the codes")})
     @RequestMapping(value = "/", consumes = {"application/json"}, method = RequestMethod.POST)
-    ResponseEntity<?> registerEPCCodes(
+    ResponseEntity<?> registerEpcCodes(
             @ApiParam(value = "Order Id with EPC codes", required = true) @RequestBody EpcCodes epcCodes,
             @ApiParam(name = "Authorization", value = "OpenID Connect token containing identity of requester", required = true) @RequestHeader(value = "Authorization") String bearer) throws IOException, UnirestException {
 
@@ -53,17 +49,17 @@ public class EpcController {
             epcCodes.setCodes(newCodes);
         }
 
-        epcCodes = epcCodesRepository.save(epcCodes);
+        epcCodesRepository.save(epcCodes);
 
         return ResponseEntity.ok().build();
     }
 
-    @ApiOperation(value = "Get EPC codes for an order.", nickname = "getEPCCodes", response = EpcCodes.class)
+    @ApiOperation(value = "Get EPC codes for an order.", nickname = "getEpcCodes", response = EpcCodes.class)
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = "EPC codes found"),
             @ApiResponse(code = 400, message = "Error while querying the codes")})
     @RequestMapping(value = "/{orderId}", method = RequestMethod.GET)
-    ResponseEntity<?> getEPCCodes(
+    ResponseEntity<?> getEpcCodes(
                     @ApiParam(value = "orderId", required = true) @PathVariable String orderId,
                     @ApiParam(name = "Authorization", value = "OpenID Connect token containing identity of requester", required = true) @RequestHeader(value = "Authorization") String bearer) throws IOException, UnirestException {
 
@@ -78,12 +74,12 @@ public class EpcController {
         return ResponseEntity.ok(epcCodes);
     }
 
-    @ApiOperation(value = "Delete EPC codes for an order and returns updated object.", nickname = "deleteEPCCodes", response = EpcCodes.class)
+    @ApiOperation(value = "Delete EPC codes for an order and returns updated object.", nickname = "deleteEpcCodes", response = EpcCodes.class)
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = "EPC codes deleted"),
             @ApiResponse(code = 400, message = "Error while deleting the codes")})
     @RequestMapping(value = "/", consumes = {"application/json"}, method = RequestMethod.DELETE)
-    ResponseEntity<?> deleteEPCCodes(
+    ResponseEntity<?> deleteEpcCodes(
             @ApiParam(value = "EPC codes object", required = true) @RequestBody EpcCodes epcCodes,
             @ApiParam(name = "Authorization", value = "OpenID Connect token containing identity of requester", required = true) @RequestHeader(value = "Authorization") String bearer) throws IOException, UnirestException {
 
